586efdecf4f89658f0d29d09616e25d27048f5ef
Ayush Kansyakar <kansyakarayush@gmail.com>
2025-10-03 19:15:12 +0530

    Add files via upload

---
+++ Project2(a).py
@@ -0,0 +1,6 @@
+N=input("input any number : ")
+N=int(N)
+if N%2==0:
+    print(N,"is an even number")
+else :
+  print(N,"is an odd number")
---
+++ Project2(b).py
@@ -0,0 +1,8 @@
+n= 50
+i=1
+s=0
+for i in range(1,51):
+    s=s+i
+    i+=1
+if (n==50):
+    print("The sum of numbers from 1 to 50 is : ",s)
